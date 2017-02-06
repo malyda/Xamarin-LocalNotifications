4c9d08e36173ea3e58f8a0743980665c31beb660
maly <maly.david.it@gmail.com>
2017-02-06 15:19:39 +0100

    Edited:
    - Android Native notifications

--- LocalNotifications/LocalNotifications/LocalNotifications/App.cs
+++ LocalNotifications/LocalNotifications/LocalNotifications/App.cs
@@ -13,37 +13,66 @@ namespace LocalNotifications
     public class App : Application
     {
         Label label = new Label();
-        public App()
+        Button b;
+    public App()
         {
-           
+           b = new Button
+           {
+               Text = "Show notification after 5 secs,",
+               Command = ShowNotifi(DateTime.Now.AddSeconds(5))
+           };
             // The root page of your application
 
-            var content = new ContentPage
+            StackLayout layout = new StackLayout
             {
-                Title = "LocalNotifications",
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                    new Label
+                    {
+                        Text = "Crossplatform notification"
+                    },
                         new Button
                         {
                             Text = "Show notification",
                             Command = ShowNotifi()
                         },
-                        new Button
-                        {
-                            Text = "Show notification after 5 secs,",
-                            Command = ShowNotifi(DateTime.Now.AddSeconds(5))
-                        },
                         label
-                    }
+
                 }
             };
+            if(Device.OS == TargetPlatform.Android)
+            {
+                layout.Children.Add(
+                    new Label
+                    {
+                        Text = "Android Only notification"
+                    });
+                layout.Children.Add(
+                    new Button
+                    {
+                        Text = "Native Android Notification",
+                        Command = ShowNotifiAndroidNative()
+                    });
+                layout.Children.Add(
+                    new Button
+                    {
+                        Text = "Android delayed notification",
+                        Command = ShowNotifi(DateTime.Now.AddSeconds(5))
+                    });     
+            }
+            
+            var content = new ContentPage
+            {
+                Title = "LocalNotifications",
+                Content = layout
+            };
+            
 
+           
 
 
             MainPage = new NavigationPage(content);
-            
+           
          
         }
 
@@ -63,10 +92,22 @@ namespace LocalNotifications
               
             }));
         }
+        Command ShowNotifiAndroidNative()
+        {
+            return new Command((() =>
+            {
+                Dictionary<string, string> data = new Dictionary<string, string>();
+                data.Add("data1", "value");
+                data.Add("data2", "value2");
+                DependencyService.Get<INotificationHelper>().Notify("title","body",data);
+
+            }));
+        }
         Command ShowNotifi(DateTime date)
         {
             return new Command((() =>
             {
+                Debug.WriteLine(date);
                 CrossLocalNotifications.Current.Show("Good morning", "Time to get up!", 1, date);
             }));
         }
